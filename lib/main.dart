602710bdecaaa4f41adf66a9d6e2deae384090a5
amir1430 <46561556+amir1430@users.noreply.github.com>
2019-05-20 18:27:46 +0430

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -36,67 +36,73 @@ class _APIAppState extends State<APIApp> {
       title: 'api',
       home: Scaffold(
         appBar: AppBar(),
-        body: ListView.builder(
-          itemCount: data == null ? 0 : data.length,
-          itemBuilder: (BuildContext context, int index) {
-            var url = data[index]['url'];
-            var id = data[index]['id'];
-            return Padding(
-              padding: EdgeInsets.all(5),
-              child: Card(
-                color: Colors.grey[300],
-                elevation: 8,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(25),
+        body: RefreshIndicator(
+          onRefresh: _handleRefresh,
+          child: ListView.builder(
+            itemCount: data == null ? 0 : data.length,
+            itemBuilder: (BuildContext context, int index) {
+              var url = data[index]['url'];
+              var id = data[index]['id'];
+              return Padding(
+                padding: EdgeInsets.all(5),
+                child: Card(
+                  color: Colors.grey[300],
+                  elevation: 8,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(25),
+                    ),
                   ),
-                ),
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(5),
-                      child: Container(
-                        height: 200,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: NetworkImage(url), fit: BoxFit.cover),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(25),
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(5),
+                        child: Container(
+                          height: 200,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: NetworkImage(url), fit: BoxFit.cover),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(25),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Text(
-                          'ID: $id',
-                          style: TextStyle(
-                              fontSize: 18,
-                              fontStyle: FontStyle.italic,
-                              fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.center,
-                        ),
-                        Text(
-                          data[index]['title'],
-                          overflow: TextOverflow.clip,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontSize: 15,
-                              
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(bottom: 5),
-                    ),
-                  ],
+                      Column(
+                        children: <Widget>[
+                          Text(
+                            'ID: $id',
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontStyle: FontStyle.italic,
+                                fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            data[index]['title'],
+                            overflow: TextOverflow.clip,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 5),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            );
-          },
+              );
+            },
+          ),
         ),
       ),
     );
+  }
+
+  Future<Null> _handleRefresh() async {
+    await new Future.delayed(new Duration(milliseconds: 1500));
+    return null;
   }
 }
